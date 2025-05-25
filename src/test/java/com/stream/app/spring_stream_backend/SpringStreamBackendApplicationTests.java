7d96610867726ceae4cbbc0ac45717f549a17302
Prajjwal Dwivedi <pr20421476@wipro.com>
2025-05-26 01:13:29 +0530

    Initial commit

---
+++ src/test/java/com/stream/app/spring_stream_backend/SpringStreamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stream.app.spring_stream_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStreamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
